340d8649f6a9513907e8ecdf657f2e2772693714
Lossica <Lossica@DESKTOP-BF8OQL9>
2017-09-04 13:51:59 -0300

    Criação de grupos para edição e inclusão.

---
+++ estoque-angular/public/js/controllers/grupos-controller.js
@@ -0,0 +1,13 @@
+angular.module('estoque').controller('GruposController', function($scope, $http){
+	
+	$scope.grupos = [];
+	$scope.mensagem = '';
+	
+	$http.get('v1/grupos')
+	.success(function(grupos){
+		$scope.grupos = grupos;
+	})
+	.error(function(erro){
+		console.log(erro);
+	});
+});
